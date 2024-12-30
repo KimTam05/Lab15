618dced6d30ec9849bee9c421c44d4662d816e28
KimTam05 <kimngoctam15@gmail.com>
2024-12-31 01:31:11 +0700

    Add slide into home page

--- resources/views/KNTuser/KNTabout.blade.php
+++ resources/views/KNTuser/KNTabout.blade.php
@@ -3,22 +3,28 @@
 @section('content')
     <div class="container">
         <h1 class="fs-1 text-center m-3">Thông Tin</h1>
-        Chào mừng bạn đến với BonsaiKT, nơi hội tụ vẻ đẹp tinh hoa của nghệ thuật bonsai! <br>
+        <p>Chào mừng bạn đến với BonsaiKT, nơi hội tụ vẻ đẹp tinh hoa của nghệ thuật bonsai!</p>
 
-        Chúng tôi chuyên cung cấp những cây bonsai độc đáo, được uốn nắn tỉ mỉ và chăm sóc kỹ lưỡng, mang đến cho bạn không chỉ là một chậu cây mà còn là một tác phẩm nghệ thuật sống động. Với niềm đam mê và sự tận tâm, chúng tôi mong muốn giúp bạn sở hữu những cây bonsai tuyệt đẹp để làm đẹp không gian sống và thể hiện phong cách riêng của mình. <br>
+        <p>Chúng tôi chuyên cung cấp những cây bonsai độc đáo, được uốn nắn tỉ mỉ và chăm sóc kỹ lưỡng, mang đến cho bạn không chỉ là một chậu cây mà còn là một tác phẩm nghệ thuật sống động. Với niềm đam mê và sự tận tâm, chúng tôi mong muốn giúp bạn sở hữu những cây bonsai tuyệt đẹp để làm đẹp không gian sống và thể hiện phong cách riêng của mình.</p>
 
         Tầm nhìn và Sứ mệnh <br>
-        Tầm nhìn: Trở thành địa chỉ hàng đầu về cây bonsai tại Việt Nam, nơi khách hàng tìm thấy giá trị nghệ thuật và sự kết nối với thiên nhiên. <br>
-        Sứ mệnh: Mang đến những tác phẩm bonsai hoàn mỹ, giúp mọi người gần gũi hơn với thiên nhiên và nuôi dưỡng sự bình yên trong tâm hồn. <br>
-        Tại sao bạn nên chọn chúng tôi? <br>
-        🌳 Bộ sưu tập đa dạng: Từ bonsai mini để bàn đến những tác phẩm bonsai lớn, phù hợp với mọi không gian và sở thích. <br>
-        🌱 Chất lượng đảm bảo: Mỗi cây bonsai đều được tuyển chọn và chăm sóc bởi các nghệ nhân giàu kinh nghiệm.
-        🎋 Tư vấn tận tình: Đội ngũ của chúng tôi luôn sẵn sàng hướng dẫn bạn cách chăm sóc bonsai một cách hiệu quả. <br>
-        🚚 Dịch vụ chuyên nghiệp: Giao hàng nhanh chóng, đảm bảo cây đến tay bạn trong tình trạng tốt nhất. <br>
+        <ul style="list-style-type:none">
+            <li>Tầm nhìn: Trở thành địa chỉ hàng đầu về cây bonsai tại Việt Nam, nơi khách hàng tìm thấy giá trị nghệ thuật và sự kết nối với thiên nhiên.</li>
+            <li>Sứ mệnh: Mang đến những tác phẩm bonsai hoàn mỹ, giúp mọi người gần gũi hơn với thiên nhiên và nuôi dưỡng sự bình yên trong tâm hồn</li>
+        </ul>
+        Tại sao bạn nên chọn chúng tôi?
+        <ul style="list-style-type:none">
+            <li>🌳 Bộ sưu tập đa dạng: Từ bonsai mini để bàn đến những tác phẩm bonsai lớn, phù hợp với mọi không gian và sở thích.
+            <li>🌱 Chất lượng đảm bảo: Mỗi cây bonsai đều được tuyển chọn và chăm sóc bởi các nghệ nhân giàu kinh nghiệm</li>
+            <li>🎋 Tư vấn tận tình: Đội ngũ của chúng tôi luôn sẵn sàng hướng dẫn bạn cách chăm sóc bonsai một cách hiệu quả.</li>
+            <li>🚚 Dịch vụ chuyên nghiệp: Giao hàng nhanh chóng, đảm bảo cây đến tay bạn trong tình trạng tốt nhất.</li></li>
+        </ul>
 
-        Hãy để bonsai thổi hồn vào không gian của bạn <br>
-        Bonsai không chỉ là cây cảnh mà còn là biểu tượng của sự tĩnh lặng, kiên nhẫn và nghệ thuật sống. Đến với chúng tôi, bạn sẽ tìm thấy những cây bonsai không chỉ đẹp mà còn mang ý nghĩa sâu sắc. <br>
+        <p>
+            Hãy để bonsai thổi hồn vào không gian của bạn <br>
+            Bonsai không chỉ là cây cảnh mà còn là biểu tượng của sự tĩnh lặng, kiên nhẫn và nghệ thuật sống. Đến với chúng tôi, bạn sẽ tìm thấy những cây bonsai không chỉ đẹp mà còn mang ý nghĩa sâu sắc.
+        </p>
 
-        Liên hệ ngay để khám phá bộ sưu tập bonsai tinh tế nhất! <br>
+        <p>Liên hệ ngay để khám phá bộ sưu tập bonsai tinh tế nhất!</p>
     </div>
 @endsection
